8d8c2ceca280f3e8aa96ef9430167dd56c724fe9
shifacodingal <shifacodingal@gmail.com>
2025-12-12 11:41:06 +0530

    Addition

---
+++ Addition.java
@@ -0,0 +1,8 @@
+class Addition{
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2  =20;
+        int sum = num1 + num2;
+        System.out.println("Addition of "+num1+" and "+num2+" is : "+sum);
+    }
+}
