12afd57c2da431fed0a6706217039a41fe9a5382
shifacodingal <shifacodingal@gmail.com>
2025-12-12 12:12:11 +0530

    Maggie

---
+++ Maggie.java
@@ -0,0 +1,14 @@
+class Maggie{
+    public static void main(String[] args) {
+        System.out.println("My Maggie Recipe");
+
+    }
+    public static void Maggie_Recipe(){
+        System.out.println("1. Boil water in pan.");
+        System.out.println("2. Add Veggies to the pan .");
+        System.out.println("3. Add Magic Masala to the veggies and add water.");
+        System.out.println("4. Bring water to boil.");
+        System.out.println("5. Add maggie to it and wait for 2 minutes .");
+        System.out.println("6. Serve it while it is hot");
+    }
+}
