792cb0127ae3de3dab6030e62932b0265e5f19ad
shifacodingal <shifacodingal@gmail.com>
2025-12-13 12:40:55 +0530

    Leap year

---
+++ LeapYear.java
@@ -0,0 +1,13 @@
+class LeapYear{
+    public static void main(String[] args) {
+        int year = 2016;
+        if (year%4==0){
+            System.out.println(year+" is a Leap year.");
+        }
+        else{
+            System.out.println(year + " is not a Leap year.");
+        }
+        
+        
+    }
+}
