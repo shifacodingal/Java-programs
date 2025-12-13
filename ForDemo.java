b81ee3ef22767aa1d0e5f2ee903720914652e0b0
shifacodingal <shifacodingal@gmail.com>
2025-12-13 21:07:14 +0530

    For Demo

---
+++ ForDemo.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+class ForDemo{
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        for(int i = 0; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
