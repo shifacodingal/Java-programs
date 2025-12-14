25cb31060cc66c23fcaf4013b42c1a632bafe344
shifacodingal <shifacodingal@gmail.com>
2025-12-14 22:59:27 +0530

    Prime Factor

---
+++ PrimeFactor.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+class PrimeFactor{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    
+        for(int div = 2; div * div <=n; div++){
+             while(n % div == 0){  
+                n  = n/div; 
+                System .out.println(div+" ");
+             } 
+        }               
+            
+        
+        if(n != 1){
+             System .out.print(n);
+        }
+         
+    }
+}
