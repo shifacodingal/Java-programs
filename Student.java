817a7f38ada0c636cfb89be3b12b68d884772865
shifacodingal <shifacodingal@gmail.com>
2026-07-08 22:34:22 +0530

    Student Register

---
+++ Student.java
@@ -0,0 +1,26 @@
+class Parent {
+
+    int age, id;
+    String name;
+
+    void naming(String name) {
+        System.out.println("Name:" + name);
+    }
+}
+
+//child class
+class Child extends Parent {
+
+    void ageN(int age) {
+        System.out.println("Age of student is:" + age);
+    }
+}
+
+class Main {
+
+    public static void main(String[] er) {
+        Child s = new Child();//creating object of child class
+        s.naming("Aashish");
+        s.ageN(14);
+    }
+}
